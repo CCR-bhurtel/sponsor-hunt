160a707c1aa6e23fd3bfc489cb53d28f0f5a7306
cceer <sisir.sital@gmail.com>
2023-01-20 22:35:50 +0545

    fix: esling disabled for build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
     reactStrictMode: false,
     swcMinify: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     env: {
         NEXT_PUBLIC_STRIPE_SECRET_KEY:
             'sk_test_51Hp8gKEhskqVAwrw1vcbenMyKucfDgRngHp5dDA6psNscmYE72N6lgZ2YEHtENKVOUivutURS750ifrMfxVN0SCX00RTiQLTf3',
